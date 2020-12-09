69a9101114ec1217981482080a5cf05c2d6e5445
Oscar Icochea Calenzani <o.icochea09@gmail.com>
2020-12-08 22:29:38 -0500

    added nav and links

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,8 +1,16 @@
 import React from "react"
-
+import {Link} from "react-router-dom"
 
 const Nav = (props) => {
-  return <h1>Nav</h1>
+  return <header>
+    <h1>App title</h1>
+    <nav>
+      <Link to ="/"><div>Home</div></Link>
+      <Link to ="/auth/signup"><div>Signup</div></Link>
+      <Link to ="/auth/login"><div>Login</div></Link>
+      {/* <Link to ="/"><div>Home</div></Link> */}
+    </nav>
+  </header>
 }
 
 export default Nav
